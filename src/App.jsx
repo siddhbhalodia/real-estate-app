70dfbecaf1bcd3e93b13d62fd4c963ab08101e06
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-04-18 23:27:49 +0530

    added react router dom and created pages(routes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import Navbar from "./components/navbar/Navbar"
-import "./layout.scss"
+// import "./layout.scss"
 import HomePage from "./routes/homePage/homePage"
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage/>
+        },
+        {
+          path: "/list",
+          element: <ListPage/>
+        },
+        {
+          path: "/:id",
+          element: <SinglePage/>
+        }
+      ]
+    }
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-      <Navbar/>
-      </div>
-      <div className="content">
-        <HomePage/>
-      </div>
-      
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/routes/singlePage/singlePage.jsx
@@ -0,0 +1,9 @@
+import "./singlePage.scss"
+
+export default function SinglePage() {
+  return (
+    <div className="singlePage">
+        SinglePage
+    </div>
+  )
+}
