999a075906960541c52907d5a93301a8071ff90f
Galani Aditya <galaniaaditya@gmail.com>
2024-04-18 17:42:01 +0530

    added Homepage design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import "./navbar.scss"
 
 function Navbar(){
+    const[open,setOpen] = useState(false)
     return (
         <nav>
             <div className="left">
@@ -16,6 +18,18 @@ function Navbar(){
             <div className="right">
                 <a href="/">Sign in</a>
                 <a href="/" className="register">Sign up</a>
+                <div className="menuIcon"> 
+                    <img src="/menu.png" alt="" onClick = {()=>setOpen((prev) => !prev)}/> 
+                </div>
+
+                <div className={open ? "menu active" : "menu                    "}>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+                <a href="/">Sign in</a>
+                <a href="/">Sign up</a>
+                </div>
             </div>
         </nav>
     )
