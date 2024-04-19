92a2c0adfd4c5ff055faa0b74d31f8d2195edb61
Galani Aditya <galaniaaditya@gmail.com>
2024-04-19 18:30:51 +0530

    added listpage

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,8 +1,22 @@
+import Filter from "../../components/filter/Filter"
+import { listData } from "../../lib/dummydata"
 import "./listPage.scss"
-
+import Card from "../../components/card/Card"
 function ListPage(){
+
+    const data = listData
     return (
-        <div className="listPage">ListPage</div>
+        <div className="listPage">
+            <div className="listContainer">
+                <div className="wrapper">
+                    <Filter/>
+                    {data.map(item=>(
+                        <Card key = {item.id} item = {item}/>
+                    ))}
+                </div>
+            </div>
+            <div className="mapContainer">Map</div>
+        </div>
     )
 }
 
